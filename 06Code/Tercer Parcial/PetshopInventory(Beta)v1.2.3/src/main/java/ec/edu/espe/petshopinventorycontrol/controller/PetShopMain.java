bc199de442707d6ec676f5ac168c48381e3edf5f
bsgudino1416 <bsgudino@espe.edu.ec>
2026-01-06 23:14:52 -0500

    Beta v1.2.3

---
+++ 06Code/Tercer Parcial/PetshopInventory(Beta)v1.2.3/src/main/java/ec/edu/espe/petshopinventorycontrol/controller/PetShopMain.java
@@ -0,0 +1,34 @@
+package ec.edu.espe.petshopinventorycontrol.controller;
+
+import ec.edu.espe.petshopinventorycontrol.employee.view.FrmLoginPetshop; 
+
+public class PetShopMain {
+
+    public static void main(String[] args) {
+        
+        // 1. Opcional: Iniciar conexión a Base de Datos (si usas Mongo o archivos globales)
+        DataEmployee.conectar(); 
+
+        // 2. Configurar el diseño visual (Look and Feel) - Opcional para que se vea moderno
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(PetShopMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        // 3. ¡LO IMPORTANTE!: Arrancar la Ventana de Login
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                // Aquí creamos y mostramos la ventana de Login
+                new FrmLoginPetshop().setVisible(true);
+            }
+        });
+        
+        System.out.println("Sistema Gráfico Iniciado.");
+    }
+}
